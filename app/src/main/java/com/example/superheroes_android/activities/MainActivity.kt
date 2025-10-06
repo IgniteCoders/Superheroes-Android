7c77e29a8b2226abc0799f68ba9e957a7173dae5
IgniteCoders <theboom.kajas@gmail.com>
2025-10-06 11:17:52 +0200

    Created the data classes and the service interface for the API calls using retrofit.

--- app/src/main/java/com/example/superheroes_android/activities/MainActivity.kt
+++ app/src/main/java/com/example/superheroes_android/activities/MainActivity.kt
@@ -1,10 +1,16 @@
-package com.example.superheroes_android
+package com.example.superheroes_android.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.superheroes_android.R
+import com.example.superheroes_android.data.SuperheroService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +21,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val service = SuperheroService.getInstance()
+            val result = service.getSuperheroById("70")
+            Log.i("API", result.toString())
         }
     }
 }
